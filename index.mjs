74239576dc7b8320b78e5c4a1e532a8a4a129be0
Kodyn Kugis <kodyn.s.kugis@gmail.com>
2022-03-28 18:37:42 +1000

    Initial command handler file structure

--- index.mjs
+++ index.mjs
@@ -12,14 +12,41 @@ client.on("ready", () => {
 
   const guildID = "950334962635243590";
   const guild = client.guilds.cache.get(guildID);
+  // let commands;
+
+  // if (guild) {
+  //   commands = guild.commands;
+  // } else {
+  //   commands = client.application?.commands;
+  // }
+
+  // commands?.create({
+  //   name: "ping",
+  //   description: "Replies with pong.",
+  // });
 });
 
-client.on("messageCreate", (message) => {
-  if (message.content === "ping") {
-    message.reply({
-      content: "pong",
-    });
-  }
-});
+// client.on("messageCreate", (message) => {
+//   if (message.content === "ping") {
+//     message.reply({
+//       content: "pong",
+//     });
+//   }
+// });
+
+// client.on("interactionCreate", async (interaction) => {
+//   if (!interaction.isCommand()) {
+//     return;
+//   }
+
+//   const { commandName, options } = interaction;
+
+//   if (commandName === "ping") {
+//     interaction.reply({
+//       content: "pong",
+//       ephemeral: true,
+//     });
+//   }
+// });
 
 client.login(process.env.TOKEN);
