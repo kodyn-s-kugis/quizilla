6afc8d528e19fe1b3eca2ea7bf48cb7cbdaedb61
Kodyn Kugis <kodyn.s.kugis@gmail.com>
2022-03-30 17:07:08 +1000

    Setup command handler

---
+++ commandHandler.js
@@ -0,0 +1,24 @@
+const fs = require("node:fs");
+const { Client, Collection, Intents } = require("discord.js");
+
+module.exports = () => {
+  const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+  client.commands = new Collection();
+
+  const commandFiles = fs
+    .readdirSync("./commands")
+    .filter((file) => file.endsWith(".js"));
+
+  console.log(commandFiles);
+
+  for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+
+    // Set a new item in the Collection
+    // With the key as the command name and the value as the exported module
+    client.commands.set(command.data.name, command);
+  }
+
+  return client;
+};
---
+++ deployCommands.js
@@ -0,0 +1,29 @@
+const fs = require("node:fs");
+const dotenv = require("dotenv");
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord-api-types/v9");
+
+dotenv.config();
+
+const commands = [];
+const commandFiles = fs
+  .readdirSync("./commands")
+  .filter((file) => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+
+  // Set a new item in the Collection
+  // With the key as the command name and the value as the exported module
+  commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: "9" }).setToken(process.env.TOKEN);
+
+rest
+  .put(
+    Routes.applicationGuildCommands(process.env.CLIENTID, process.env.GUILDID),
+    { body: commands }
+  )
+  .then(() => console.log("Successfully registered application commands."))
+  .catch(console.error);
