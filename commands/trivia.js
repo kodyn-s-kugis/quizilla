83f5858549dd433d1c96ee2eb463d260068113ab
Kodyn Kugis <kodyn.s.kugis@gmail.com>
2022-03-30 17:07:28 +1000

    Create subcommands for trivia

---
+++ commands/trivia.js
@@ -0,0 +1,24 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("trivia")
+    .setDescription("Manage your trivia games")
+    .addSubcommand((subcommand) =>
+      subcommand.setName("create").setDescription("Create a trivia game!")
+    )
+    .addSubcommand((subcommand) =>
+      subcommand.setName("close").setDescription("Close your trivia game!")
+    ),
+  async execute(interaction) {
+    if (interaction.options.getSubcommand() === "create") {
+      await interaction.reply(
+        `${interaction.user.username}, we are creating your trivia game now!`
+      );
+    } else if (interaction.options.getSubcommand() === "close") {
+      await interaction.reply(
+        `${interaction.user.username}, we are closing your trivia game, hope you had a good time!`
+      );
+    }
+  },
+};
