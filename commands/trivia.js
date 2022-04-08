82bfa7489cf350b26b061741210e70cd28d6170d
Kodyn Kugis <kodyn.s.kugis@gmail.com>
2022-04-08 20:36:02 +1000

    Added difficulty option

--- commands/trivia.js
+++ commands/trivia.js
@@ -6,18 +6,28 @@ module.exports = {
     .setName("trivia")
     .setDescription("Manage your trivia games")
     .addSubcommand((subcommand) =>
-      subcommand.setName("create").setDescription("Create a trivia game!")
+      subcommand
+        .setName("create")
+        .setDescription("Create a trivia game!")
+        .addStringOption((option) =>
+          option
+            .setName("difficulty")
+            .setDescription("Set question difficulty")
+            .setRequired(true)
+            .addChoice("Easy", "easy")
+            .addChoice("Medium", "medium")
+            .addChoice("Hard", "hard")
+        )
     )
     .addSubcommand((subcommand) =>
       subcommand.setName("close").setDescription("Close your trivia game!")
     ),
   async execute(interaction) {
     if (interaction.options.getSubcommand() === "create") {
+      let difficulty = interaction.options.getString("difficulty");
       // Here goes all functionality, all the commands
-
       await interaction.reply(
-        //`${interaction.user.username}, we are creating your trivia game now!`,
-        await MCQuestions(interaction)
+        `${interaction.user.username}, we are creating your trivia game in ${difficulty} difficulty!`
       );
       await interaction.followUp(await questionsMC());
     } else if (interaction.options.getSubcommand() === "close") {
--- services/data/questionsData.js
+++ services/data/questionsData.js
@@ -1,26 +1,39 @@
 module.exports = {
   themes: [
     {
-      history: [
-        {
-          question: "What year was the first iPhone released?",
-          answers: [2005, 2006, 2007, 2009],
-          correct: 2007,
-          askedBefore: false,
-        },
-        {
-          question:
-            "Which Australian prime minister was preceded and succeeded by Kevin Rudd?",
-          answers: [
-            "Scott morrison",
-            "Tony Abbott",
-            "Julia Gillard",
-            "John Howard",
+      history: {
+        easy: {
+          questions: [
+            {
+              question: "What year was the first iPhone released?",
+              answers: [2005, 2006, 2007, 2009],
+              correct: 2007,
+              askedBefore: false,
+            },
+            {
+              question:
+                "Which Australian prime minister was preceded and succeeded by Kevin Rudd?",
+              answers: [
+                "Scott morrison",
+                "Tony Abbott",
+                "Julia Gillard",
+                "John Howard",
+              ],
+              correct: "Julia Gillard",
+              askedBefore: false,
+            },
           ],
-          correct: "Julia Gillard",
-          askedBefore: false,
+          points: 10,
         },
-      ],
+        medium: {
+          questions: [],
+          points: 10,
+        },
+        hard: {
+          questions: [],
+          points: 10,
+        },
+      },
     },
   ],
 };
