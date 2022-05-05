7e9f49afe03202c058ee79cf1e84eb86e059dc22
Kodyn Kugis <kodyn.s.kugis@gmail.com>
2022-05-05 18:50:21 +1000

    Added Queue Functionality

--- services/playerSignUp.js
+++ services/playerSignUp.js
@@ -35,15 +35,30 @@ module.exports = async function playerSignUp(interaction, gameData) {
 
   const collector = message.createMessageComponentCollector({
     componentType: "BUTTON",
-    time: 1000 * 60,
+    time: 1000 * 120,
   });
 
   collector.on("collect", (i) => {
     if (i.customId === "queue") {
-      console.log("queue");
+      // if no user has queued
+      if (game.players.length < 1) {
+        queueButtonHandler(i.user.id);
+
+        i.reply({ content: `You've queued to play Trivia`, ephemeral: true });
+      } else if (game.players.find((player) => player?.id !== i.user.id)) {
+        queueButtonHandler(i.user.id);
+
+        i.reply({ content: `You've queued to play Trivia`, ephemeral: true });
+      } else {
+        i.reply({
+          content: `You've already queued to play Trivia`,
+          ephemeral: true,
+        });
+      }
     }
     if (i.customId === "unqueue") {
-      console.log("unqueue");
+      // if user hasn't queued, but tries to unqueue
+      i.reply({ content: `You've unqueued from Trivia`, ephemeral: true });
     }
   });
 
@@ -51,7 +66,29 @@ module.exports = async function playerSignUp(interaction, gameData) {
     console.log(`Collected ${collected.size} items`)
   );
 
-  async function queueButtonHandler() {}
+  async function queueButtonHandler(userID) {
+    game.players.push({ id: userID, points: 0 });
+    console.log(game.players);
 
-  async function unqueueButtonHandler() {}
+    await updateSignUpEmbed();
+  }
+
+  function unqueueButtonHandler(userID) {}
+
+  async function updateSignUpEmbed() {
+    const queuedPlayers = game.players
+      .map((player) => `<@${player.id}>`)
+      .join("\n");
+    console.log(queuedPlayers);
+
+    const upatedEmbed = new MessageEmbed()
+      .setColor("#0099ff")
+      .setTitle(`Trivia Game`)
+      .setDescription(`Created by ${interaction.user.tag}`)
+      .addFields({ name: `Queued Players`, value: `${queuedPlayers}` });
+
+    await message.edit({
+      embeds: [upatedEmbed],
+    });
+  }
 };
